ef82bc3b13c4fd24256a7fef5f0bfbbbf63f1b7a
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-11-10 19:08:05 +0600

    reviwes data added

--- index.js
+++ index.js
@@ -37,6 +37,27 @@ async function run(){
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
+
+        // reviwe api 
+
+        app.get('/reviews', async(req, res) => {
+            let query = {};
+
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            const result = reviewCollection.insertOne(review);
+            res.send(result)
+        })
     }
     finally{
 

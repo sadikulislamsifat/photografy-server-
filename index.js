2dec8a63bc0556403aab01b898d69d11b94f9945
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-11-10 21:36:05 +0600

    delete review btn implement

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ async function run(){
 
         app.get('/reviews', async(req, res) => {
             let query = {};
-
             if(req.query.email){
                 query = {
                     email: req.query.email
@@ -56,6 +55,13 @@ async function run(){
         app.post('/reviews', async(req, res) => {
             const review = req.body;
             const result = reviewCollection.insertOne(review);
+            res.send(result)
+        });
+        
+        app.delete('/reviews/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
             res.send(result)
         })
     }
